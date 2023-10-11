81320fea0cae7fac6de118cd8d688815a635ffb1
DiegoApizza <diegoantonio40@live.com>
2023-10-10 22:03:04 -0300

    mejopras
    ANDA EL VALIDTOR

--- js/register.js
+++ js/register.js
@@ -1,23 +1,24 @@
 //Validacion de Formulario
-const nombre=document.createElement("nombre")
-const email=document.createElement("email")
-const password=document.createElement("password")
+const nombre = document.createElement("nombre")
+const email = document.createElement("email")
+const password = document.createElement("password")
 
 //TomarDAtos
-const objetoFormulario=document.querySelector('form')
-    .addEventListener('submit',(e)=>{
-        e.preventDefault()
-        const datos=Object.fromEntries(
-            new FormData (e.target)
-        )
-        const expresionReg=/^\w+([\.-]?w+)*@\w+([\.-]?\w+)*(\.w{2,4})+$/   
-        console.log({datos})
-        
-        
-        if(!expresionReg.test(email.value)){
-            alert("Escriba Un Mail Valido")
-        }
-        if(password.value.length<8){
-            alert("Contraseña muy corta")
-        }
-    })
+const objetoFormulario = document.querySelector('form');
+
+objetoFormulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const datos = Object.fromEntries(new FormData(e.target));
+
+    const expresionReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+
+    console.log({ datos });
+
+    if (!expresionReg.test(datos.email)) {
+        alert("Escriba un correo válido");
+    }
+    if (datos.password.length < 8) {
+        alert("Contraseña muy corta");
+    }
+});
