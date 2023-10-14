9157309fdf4414bbb7246b294c371d22ca4b100d
KarenVares26 <ares26.k@gmail.com>
2023-10-14 19:31:29 -0300

    Validacion

--- js/FormCompra.js
+++ js/FormCompra.js
@@ -1,21 +1,17 @@
-var direccion =document.getElementById("inputAddress");
-var ciudad = document.getElementById("inputCity")
-var postal = document.getElementById("inputZip")
-var numTarjeta= document.getElementById("inputCard")
-var formulario= document.getElementById("Form")
-//En esta Validacion verificamos que contenga Letras 
-formulario.addEventListener("submit", e=>{
-    let verficarDirecion=/\b\w+\b/
-    e.preventDefault()
-    if((direccion==null||direccion.innerHTML=="")){
-        alert("Porfavor complete la direccion")
-    }
-    if(((ciudad==null||ciudad.innerHTML=="")&& !verficarDirecion.test(ciudad))){
-        alert("Ingre una Ciudad Correcta")
-    }
-    let exprecionRegularNum= /^\d+$/
-    if(!exprecionRegularNum.test(numTarjeta)){
-        alert("Numero de tarjeta invalido, por favor ingrese un numero valido.")
-    }
-})
+
+function validarFormulario() {
+   
+    var direccion = document.getElementById('direccion').value.trim();
+    var ciudad = document.getElementById('inputCity').value.trim();
+    var codigoPostal = document.getElementById('inputZip').value.trim();
+  
     
+    if (direccion === '' || ciudad === '' || codigoPostal === '') {
+    
+    alert('Por favor, completa todos los campos: Dirección, Ciudad y Código Postal.');
+      return false; // Evitar que se envíe el formulario
+    }
+    else
+    alert("Formulario completado correctamente")
+    return true;
+  }
